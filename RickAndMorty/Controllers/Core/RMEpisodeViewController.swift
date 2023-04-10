9c8c1365d2ceb55cbf9907353b22eddbd7e31256
mina eid <minaeid6@gmail.com>
2023-04-11 00:14:05 +0200

    Add Models

--- RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -13,7 +13,7 @@ final class RMEpisodeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBackground
+    view.backgroundColor = .systemBackground
      title = "Episodes"
         
     }
--- RickAndMorty/Models/RMCharacter.swift
+++ RickAndMorty/Models/RMCharacter.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+struct RMCharacter: Codable {
+    let id : Int
+    let name: String
+    let status : RMCharacterStatus
+    let species:String
+    let type:String
+    let gender:RMCharacterGender
+    let origin : RMOrigin
+    let location: RMLocation
+    let image:String
+    let episode: [String]
+    let url: String
+    let created: String
+    
+    
+}
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
+struct RMLocation : Codable {
+    let name: String
+    let url: String
+}
+
+enum RMCharacterStatus : String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
+enum RMCharacterGender : String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
