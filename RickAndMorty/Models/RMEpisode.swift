b3a30c104b67c873b10e7af8dfe3c57d1e64c176
mina eid <minaeid6@gmail.com>
2023-04-10 21:40:11 +0200

    Created Rick and Morty Project

---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Mina on 10/04/2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Mina on 10/04/2023.
+//
+
+import Foundation
